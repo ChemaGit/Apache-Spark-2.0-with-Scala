f9f0d65f3c86cc2deb8abe4c68346a12866c098d
Chema <josem32832@gmail.com>
2019-07-19 16:36:16 +0200

    19072019

--- src/main/scala/OOPSandFunctionalProgramming/Options.scala
+++ src/main/scala/OOPSandFunctionalProgramming/Options.scala
@@ -3,7 +3,25 @@ package OOPSandFunctionalProgramming
 object DemoOptions {
 	def main(args: Array[String]): Unit = {
 		println("""capitals.get("France"): """ + capitals.get("France"))
+		println("""capitals.get("India"): """ + capitals.get("India"))
+		println("""show(capitals.get("Japan")): """ + show(capitals.get("Japan")))
+		println("""show(capitals.get("India")): """ + show(capitals.get("India")))
+
+		println("a.getOrElse(0): " + a.getOrElse(0))
+		println("b.getOrElse(10): " + b.getOrElse(10))
+
+		println("a.isEmpty: " + a.isEmpty)
+		println("b.isEmpty: " + b.isEmpty)
 	}
 
 	val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
+	val a: Option[Int] = Some(5)
+	val b: Option[Int] = None
+
+	def show(opt: Option[String]): String = {
+		opt match {
+			case Some(s) => s
+			case None => "What the hell?"
+		}
+	}
 }
