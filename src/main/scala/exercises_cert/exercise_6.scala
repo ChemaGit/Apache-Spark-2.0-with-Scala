460aaf4eb155ff07815432e8f0c7b3fca9f0d714
Chema <josem32832@gmail.com>
2019-07-25 16:05:07 +0200

    25072019

---
+++ src/main/scala/exercises_cert/exercise_6.scala
@@ -0,0 +1,13 @@
+/** Question 8
+  * Problem Scenario 63 : You have been given below code snippet.
+  * val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"), 2)
+  * val b = a.map(x => (x.length, x))
+  * operation1
+  * Write a correct code snippet for operation1 which will produce desired output, shown below.
+  * Array[(Int, String)] = Array((4,lion), (3,dogcat), (7,panther), (5,tigereagle))
+  */
+val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"), 2)
+val b = a.map(x => (x.length, x))
+b.reduceByKey({case(v1, v2) => v1 + v2}).collect
+
+// res10: Array[(Int, String)] = Array((4,lion), (3,dogcat), (7,panther), (5,tigereagle))
