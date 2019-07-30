b35873bcf46a855b075995258d96a1ddd660d90d
Chema <josem32832@gmail.com>
2019-07-30 16:39:27 +0200

    30072019

---
+++ src/main/scala/exercises_cert/exercise_9.scala
@@ -0,0 +1,30 @@
+/** Question 13
+  * Problem Scenario 32 : You have given three files as below.
+  * spark3/sparkdir1/file1.txt
+  * spark3/sparkdir2/file2.txt
+  * spark3/sparkdir3/file3.txt
+  * Each file contain some text.
+  * spark3/sparkdir1/file1.txt
+  * Apache Hadoop is an open-source software framework written in Java for distributed
+  * storage and distributed processing of very large data sets on computer clusters built from
+  * commodity hardware. All the modules in Hadoop are designed with a fundamental
+  * assumption that hardware failures are common and should be automatically handled by the framework
+  * spark3/sparkdir2/file2.txt
+  * The core of Apache Hadoop consists of a storage part known as Hadoop Distributed File
+  * System (HDFS) and a processing part called MapReduce. Hadoop splits files into large
+  * blocks and distributes them across nodes in a cluster. To process data, Hadoop transfers
+  * packaged code for nodes to process in parallel based on the data that needs to be processed.
+  * spark3/sparkdir3/file3.txt
+  * his approach takes advantage of data locality nodes manipulating the data they have
+  * access to to allow the dataset to be processed faster and more efficiently than it would be
+  * in a more conventional supercomputer architecture that relies on a parallel file system
+  * where computation and data are distributed via high-speed networking
+  *
+  * Now write a Spark code in scala which will load all these three files from hdfs and do the
+  * word count by filtering following words. And result should be sorted by word count in reverse order.
+  * Filter words ("a","the","an", "as", "a","with","this","these","is","are","in", "for","to","and","The","of")
+  * Also please make sure you load all three files as a Single RDD (All three files must be loaded using single API call).
+  * You have also been given following codec
+  * import org.apache.hadoop.io.compress.GzipCodec
+  * Please use above codec to compress file, while saving in hdfs.
+  */
--- src/main/scala/playing_with_rdds/Actions.scala
+++ src/main/scala/playing_with_rdds/Actions.scala
@@ -5,7 +5,56 @@ import org.apache.spark.sql.SparkSession
 object Actions {
 	def main(args: Array[String]): Unit = {
 		val spark = SparkSession.builder.appName("ActionsExample").master("local").getOrCreate()
-		val sc = spark.SparkContext()
+		val sc = spark.sparkContext
 		sc.setLogLevel("ERROR")
+
+		/**********count Action***************/
+		println("***********count Action*******************")
+		val data = sc.textFile("hdfs://quickstart.cloudera/user/cloudera/files/file_1.txt")
+		val mapFile = data.flatMap(line => line.split(" ")).filter(w => w.toLowerCase == "hadoop")
+		println(mapFile.count())
+		
+		/**********collect Action***************/
+		println("************collect Action************************")
+		val rdd = sc.parallelize(Array(("A",1),("B",2),("C",3)))
+		val rdd1 = sc.parallelize(Array(("A",4),("A",6),("B",7),("C",3),("C",8)))
+		val res = rdd.join(rdd1)
+		res.collect.foreach(println)
+
+		/**********take Action***************/
+		println("************take Action************************")
+		val rdd2 = sc.parallelize(Array(("k",5),("s",3),("s",4),("p",7),("p",5),("t",8),("k",6)),3)
+		val group = rdd2.groupByKey().collect
+		val twoRec = group.take(2)
+		twoRec.foreach(println)
+
+		/**********top Action***************/
+		println("************top Action************************")
+		val rdd3 = sc.textFile("hdfs://quickstart.cloudera/user/cloudera/files/file_2.txt")
+		val mapTop = rdd3.map(line => (line, line.length))
+		val res1 = mapTop.top(3)
+		res1.foreach(println)
+
+		/**********collect countByValue***************/
+		println("************collect countByValue************************")
+		val rdd4 = sc.textFile("hdfs://quickstart.cloudera/user/cloudera/files/file_3.txt")
+		val res2 = rdd4.flatMap(line => line.split("\\W")).countByValue
+		res2.foreach(println)
+
+		/**********reduce Action***************/
+		println("************reduce Action************************")
+		val rdd5 = sc.parallelize(List(20,34,12,34,56,7,3,89,100))
+		val res3 = rdd5.reduce((v,v1) => v * v1)
+		val res4 = rdd5.reduce((v, v1) => v + v1)
+		println(s"product: $res3, sum: $res4")
+
+		/**********foreach Action***************/
+		println("************foreach Action************************")
+		val rdd6 = sc.parallelize(Array(("k",5),("s",3),("s",4),("p",7),("p",5),("t",8),("k",6)),3)		
+                val group1 = rdd6.groupByKey().collect
+                group1.foreach(println)
+
+		sc.stop()
+		spark.stop()
 	}
 }
