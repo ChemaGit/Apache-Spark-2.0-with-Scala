97f0bec7ef752279f145a3ebf25b7d82f9c264be
Chema <josem32832@gmail.com>
2019-08-14 16:45:29 +0200

    14082019

--- src/main/scala/exercises_cert_1/exercise_2.scala
+++ src/main/scala/exercises_cert_1/exercise_2.scala
@@ -10,3 +10,31 @@
   * Expected output: averageScores: scala.collection.Map[String,Double] = Map(Fred -> 91.33333333333333, Wilma -> 95.33333333333333)
   * Define all three required function , which are input for combineByKey method, e.g. (createScoreCombiner, scoreCombiner, scoreMerger). And help us producing required results.
   */
+type ScoreCollector = (Int, Double)
+type PersonScores = (String, (Int, Double))
+
+val initialScores = Array(("Fred", 88.0), ("Fred", 95.0), ("Fred", 91.0), ("Wilma", 93.0),("Wilma", 95.0), ("Wilma", 98.0))
+val wilmaAndFredScores = sc.parallelize(initialScores).cache()
+
+val averagingFunction = (personScore: PersonScores) => {
+  val (name, (numberScores, totalScore)) = personScore
+  (name, totalScore / numberScores)
+}
+
+def createScoreCombiner(i:Double): ScoreCollector = {
+  new ScoreCollector(1, i)
+}
+
+def scoreCombiner(c: ScoreCollector, v:Double): ScoreCollector = {
+  new ScoreCollector(c._1 + 1, c._2 + v)
+}
+
+def scoreMerger(c: ScoreCollector, c1: ScoreCollector): ScoreCollector = {
+  new ScoreCollector(c._1 + c1._1, c._2 + c1._2)
+}
+
+val scores = wilmaAndFredScores.combineByKey(createScoreCombiner, scoreCombiner, scoreMerger)
+
+val averageScores = scores.collectAsMap().map(averagingFunction)
+
+// averageScores: scala.collection.Map[String,Double] = Map(Fred -> 91.33333333333333, Wilma -> 95.33333333333333)
---
+++ src/main/scala/spark_sql/SparkSQLUDF.scala
@@ -0,0 +1,27 @@
+package spark_sql
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.udf
+
+
+object SparkSQLUDF {
+
+	val upper: String => String = str => str.toUpperCase
+
+	def main(args: Array[String]): Unit = {
+		val spark = SparkSession.builder.appName("Spark SQL UDF").master("local").getOrCreate()
+		val sc = spark.sparkContext
+		import spark.implicits._
+		sc.setLogLevel("ERROR")
+		
+		val dataset = Seq((0, "hello"),(1, "world")).toDF("id","text")
+		val upperUDF = udf(upper)
+		dataset.withColumn("upper", upperUDF($"text")).show()
+
+		spark.udf.register("myUpper",(input: String) => input.toUpperCase)
+		spark.catalog.listFunctions.filter('name like "%upper%").show(false)
+
+		sc.stop
+		spark.stop
+	}
+}
