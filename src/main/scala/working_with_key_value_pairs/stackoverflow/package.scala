8db08ae1e4209612a1a15460ba0393725c165130
Chema <josem32832@gmail.com>
2019-12-10 15:12:23 +0100

    20191210

---
+++ src/main/scala/working_with_key_value_pairs/stackoverflow/package.scala
@@ -0,0 +1,9 @@
+package working_with_key_value_pairs
+
+package object stackoverflow {
+  type Question = Posting
+  type Answer = Posting
+  type QID = Int
+  type HighScore = Int
+  type LangIndex = Int
+}
