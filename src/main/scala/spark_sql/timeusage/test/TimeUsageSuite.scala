2dac63822cfb9b641b3b208c16bca2fe3d17e4ee
Chema <josem32832@gmail.com>
2019-12-23 07:13:48 +0100

    20191223

---
+++ src/main/scala/spark_sql/timeusage/test/TimeUsageSuite.scala
@@ -0,0 +1,19 @@
+package spark_sql.timeusage.test
+
+import org.apache.spark.sql.{ColumnName, DataFrame, Row}
+import org.apache.spark.sql.types.{
+  DoubleType,
+  StringType,
+  StructField,
+  StructType
+}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.util.Random
+
+@RunWith(classOf[JUnitRunner])
+class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+}
